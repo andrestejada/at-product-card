ae8130bc4d725c83ceac6daf63f212e64cd7dac5
Andres <aneduardotejada@gmail.com>
2021-12-22 21:17:53 -0500

    v0.0.1 relase candidate!

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,46 +1,48 @@
-import { useEffect,  useRef,  useState } from "react"
+import { useEffect, useRef, useState } from 'react';
 import { InitialValues, OnChangeArgs, Product } from '../interfaces/index';
 
 interface useProductArg {
-    onChange?:(arg:OnChangeArgs)=>void;
-    product:Product;
-    value?:number;
-    initialValues?:InitialValues;
+  onChange?: (arg: OnChangeArgs) => void;
+  product: Product;
+  value?: number;
+  initialValues?: InitialValues;
 }
-export const useProduct =({onChange,product,value=0,initialValues}:useProductArg)=>{
+export const useProduct = ({
+  onChange,
+  product,
+  value = 0,
+  initialValues,
+}: useProductArg) => {
+  const [count, setCount] = useState<number>(initialValues?.count || value);
 
-    const [count, setCount] = useState<number>( initialValues?.count || value);
-
-    const isMount = useRef(false)
-    const increaseBy =(value:number)=>{
-
-        let newValue = Math.max( count + value,0);
-        if(initialValues?.maxCount){
-            newValue = Math.min(initialValues.maxCount,newValue)
-        }
-        setCount(newValue);
-
-        onChange && onChange({ count:newValue,product});
+  const isMount = useRef(false);
+  const increaseBy = (value: number) => {
+    let newValue = Math.max(count + value, 0);
+    if (initialValues?.maxCount) {
+      newValue = Math.min(initialValues.maxCount, newValue);
     }
+    setCount(newValue);
 
-    const reset =()=>{
-        setCount( initialValues?.count || value)
-    }
-    useEffect(() => {
-        if(!isMount.current) return;
-        setCount(value);
-    }, [value]);
+    onChange && onChange({ count: newValue, product });
+  };
 
-    useEffect(() => {        
-        isMount.current = true;
-    }, []);
-    return{
-        count,
-        isMaxCountReached: !!initialValues?.count && initialValues.maxCount === count ,
-        maxCount:initialValues?.maxCount,
-        reset,
-        increaseBy,
-    }
+  const reset = () => {
+    setCount(initialValues?.count || value);
+  };
+  useEffect(() => {
+    if (!isMount.current) return;
+    setCount(value);
+  }, [value]);
 
-}
-
+  useEffect(() => {
+    isMount.current = true;
+  }, []);
+  return {
+    count,
+    isMaxCountReached:
+      !!initialValues?.count && initialValues.maxCount === count,
+    maxCount: initialValues?.maxCount,
+    reset,
+    increaseBy,
+  };
+};
